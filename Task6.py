5cb98a531f5012582a1b43e65ff78755280d0c85
codegit-omii <omthakrey112@gmail.com>
2025-12-22 15:29:02 +0530

    Assignment 3 task 2

---
+++ Task6.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+sqrt_num = math.sqrt(num)
+print(f"The square root of {num} is: {sqrt_num}")
+log_num = math.log(num)
+print(f"The natural logarithm of {num} is: {log_num}")
+sin = math.sin(num)
+print(f"The sine of {num} is: {sin}")
